3ca4e26713068dce85576a7cb18d4196531d084b
Abril Araneda <abrilaraneda@gmail.com>
2024-03-01 21:41:30 -0300

    modified:   module5-solution/js/script.js

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -118,7 +118,7 @@ function buildAndShowHomeHTML (categories) {
       //
       // var homeHtmlToInsertIntoMainPage = ....
 
-      var category = chosenCategoryShortName;
+      var category = `"${chosenCategoryShortName}"`;
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", category);
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
